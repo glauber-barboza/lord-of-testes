b5001f6f0c191cd536b2b8c72d1605e1a40b89fa
Glauber (o)(o) <glauber.barboza@hotmail.com>
2018-08-13 09:56:54 -0300

    new spring project java

---
+++ src/main/java/br/com/lordoftests/LordOfTestsApplication.java
@@ -0,0 +1,12 @@
+package br.com.lordoftests;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LordOfTestsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LordOfTestsApplication.class, args);
+	}
+}
