609f039b13333d9d3be36cafc90713d8ef015fec
Glauber (o)(o) <glauber.barboza@hotmail.com>
2018-08-29 21:26:23 -0300

    classe de testes guerraServiceTest

---
+++ src/test/java/br/com/lordoftests/service/GuerraServiceTest.java
@@ -0,0 +1,26 @@
+package br.com.lordoftests.service;
+
+import br.com.lordoftests.factory.OrkFactory;
+import br.com.lordoftests.model.Elfo;
+import br.com.lordoftests.model.Ork;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@Import(GuerraService.class)
+public class GuerraServiceTest {
+
+    @Autowired
+    private GuerraService guerraService;
+
+    @Test
+    public void atacarOrk(){
+        Ork ork = new OrkFactory.Builder().build();
+        Elfo elfo= new Elfo(1000,500,120,0);
+
+        guerraService.matarOrk(ork,elfo);
+    }
+}
