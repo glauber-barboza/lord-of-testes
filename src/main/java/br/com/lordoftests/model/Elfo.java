48dd5f325d64e0d7ff7f89f239b1ef86f4c7589e
Glauber (o)(o) <glauber.barboza@hotmail.com>
2018-08-29 21:25:26 -0300

    Ajuste nome da classe Elfos para elfo

---
+++ src/main/java/br/com/lordoftests/model/Elfo.java
@@ -0,0 +1,7 @@
+package br.com.lordoftests.model;
+
+public class Elfo extends Personagem {
+    public Elfo(Integer dinheiro, Integer vida, Integer damage, Integer defesa) {
+        super(dinheiro, vida, damage, defesa);
+    }
+}
--- src/main/java/br/com/lordoftests/model/Elfos.java
+++
@@ -1,4 +0,0 @@
-package br.com.lordoftests.model;
-
-public class Elfos {
-}
