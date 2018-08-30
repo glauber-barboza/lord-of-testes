9930bd797bea3f7aa585f4c629062c9e69ea4a5c
Glauber (o)(o) <glauber.barboza@hotmail.com>
2018-08-29 21:27:51 -0300

    criado método matarOrk

--- src/main/java/br/com/lordoftests/service/GuerraService.java
+++ src/main/java/br/com/lordoftests/service/GuerraService.java
@@ -1,4 +1,22 @@
 package br.com.lordoftests.service;
 
+import br.com.lordoftests.model.Ork;
+import br.com.lordoftests.model.interfaces.ISoldado;
+import org.springframework.stereotype.Service;
+
+@Service
 public class GuerraService {
+
+    public Boolean matarOrk(Ork ork, ISoldado soldado){
+        System.out.println("vida do Soldado escolhido: "+soldado.getVida()+ " vida do Ork: "+ ork.getVida());
+
+        while (soldado.getVida() > 0 && ork.getVida() > 0) {
+            soldado.atacar(ork);
+            System.out.println("vida do Soldado escolhido: "+soldado.getVida()+ " vida do Ork: "+ ork.getVida());
+
+        }
+        if(ork.getVida() > 0) return false;
+
+        return true;
+    }
 }
